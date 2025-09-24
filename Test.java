6b3c23673de72679557b043ffb649515ed40dc28
daaniys-code <daaniysn@gmail.com>
2025-09-24 14:15:57 +0700

    daaniys nulis

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hi, ini code baris pertama");
+        System.out.println("hai Andra, Daaniys Mau Menulis Di Siniii");
     }
 }
